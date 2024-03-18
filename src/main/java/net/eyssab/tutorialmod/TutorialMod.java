e8f78b8bad0a0868b91903157669916594973108
EyssaB <eyssab@gmail.com>
2024-03-18 14:16:47 -0400

    tut2

--- src/main/java/net/eyssab/tutorialmod/TutorialMod.java
+++ src/main/java/net/eyssab/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package net.eyssab.tutorialmod;
 
+import net.eyssab.tutorialmod.item.ModItemGroups;
+import net.eyssab.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,10 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
 	}
 }

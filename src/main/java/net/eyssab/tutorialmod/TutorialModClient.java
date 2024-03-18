4f83d7ca0e9e550a4cd213a05bc43ed4621c30ea
EyssaB <eyssab@gmail.com>
2024-03-18 12:17:01 -0400

    initial commit

---
+++ src/main/java/net/eyssab/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package net.eyssab.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    public void onInitializeClient() {
+
+    }
+}

f8b4e557ebd61366dee15799cbce9a9b20b4c87a
EyssaB <eyssab@gmail.com>
2024-03-21 18:08:31 -0400

    tut7

--- src/main/java/net/eyssab/tutorialmod/item/ModItems.java
+++ src/main/java/net/eyssab/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.eyssab.tutorialmod.item;
 
 import net.eyssab.tutorialmod.TutorialMod;
+import net.eyssab.tutorialmod.item.custom.MetalDetectorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -14,6 +15,8 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+    public static final Item METAL_DETECTOR = registerItem("metal_detector", new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private static void addItemstoIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
